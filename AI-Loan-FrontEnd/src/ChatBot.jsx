45dadc976361d8e6f0c1c31aaa08042d4eb00e61
V-KennyPhilip <kennyphilip.velugu@gmail.com>
2025-03-06 10:57:06 +0530

    Test commit

--- AI-Loan-FrontEnd/src/ChatBot.jsx
+++ AI-Loan-FrontEnd/src/ChatBot.jsx
@@ -12,7 +12,7 @@ const Chatbot = () => {
     setMessages([
       {
         id: 1,
-        text: "Hello! How can I help you today??",
+        text: "Hello! How can I help you today???",
         sender: 'bot',
         options: [
           { id: 'products', text: 'Learn about Products' },
